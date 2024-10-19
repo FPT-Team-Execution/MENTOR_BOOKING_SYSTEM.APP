fe1c3f06b9ef5285186c4810d0089f451ce22678
DatSung <datsung.dev@gmail.com>
2024-10-19 14:50:12 +0700

    config: initial project structure

---
+++ MentorBookingSystem/MBS.Services/Models/BaseModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBS.Services.Models
+{
+    public class BaseModel<TResponseModel, TRequestModel> : IResponse where TResponseModel : class where TRequestModel : class
+    {
+        public bool IsSuccess { get; set; }
+        public required string Message { get; set; }
+        public int StatusCode { get; set; }
+        public TResponseModel? ResponseModel { get; set; }
+        public TRequestModel? RequestModel { get; set; }
+    }
+
+    public class BaseModel<TResponseRequestModel> : IResponse where TResponseRequestModel : class
+    {
+        public bool IsSuccess { get; set; }
+        public required string Message { get; set; }
+        public int StatusCode { get; set; }
+        public TResponseRequestModel? ResponseRequestModel { get; set; }
+    };
+
+    public class BaseModel : IResponse
+    {
+        public bool IsSuccess { get; set; }
+        public required string Message { get; set; }
+        public int StatusCode { get; set; }
+    };
+}
---
+++ MentorBookingSystem/MBS.Services/Models/IResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBS.Services.Models
+{
+    public interface IResponse
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public int StatusCode { get; set; }
+    }
+}
