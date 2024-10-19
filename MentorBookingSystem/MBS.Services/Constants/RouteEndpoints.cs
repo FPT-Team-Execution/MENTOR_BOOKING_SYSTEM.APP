97d3fdb1b3fc496a9c8b56531d8a1b0af7fb34a8
DatSung <datsung.dev@gmail.com>
2024-10-19 20:21:11 +0700

    feat: change asp tag helper, implement login logout

--- MentorBookingSystem/MBS.Services/Constants/RouteEndpoints.cs
+++ MentorBookingSystem/MBS.Services/Constants/RouteEndpoints.cs
@@ -2,5 +2,13 @@
 
 public class RouteEndpoints
 {
+    public static string Login = "/Login";
+    public static string Logout = "/Logout";
+    public static string Register = "/Register";
+    public static string LoginGoole = "/LoginGoogle";
+    
+    
+    // Admin routes
+    public static string Admin= "/AdminPage";
     public static string AdminDashboard = "/AdminPage/Dashboard";
 }
