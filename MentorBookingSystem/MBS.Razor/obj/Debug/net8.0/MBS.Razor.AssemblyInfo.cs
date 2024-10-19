9f3d67524d7b1a8cf773b54045b782dadfd7bae7
DatSung <datsung.dev@gmail.com>
2024-10-19 16:04:00 +0700

    feat: implment admin sb 2 dashboard

--- MentorBookingSystem/MBS.Razor/obj/Debug/net8.0/MBS.Razor.AssemblyInfo.cs
+++ MentorBookingSystem/MBS.Razor/obj/Debug/net8.0/MBS.Razor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MBS.Razor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+471d4182d1c484961a4526c4b6ff03c8bbd8a0de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe1c3f06b9ef5285186c4810d0089f451ce22678")]
 [assembly: System.Reflection.AssemblyProductAttribute("MBS.Razor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MBS.Razor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
