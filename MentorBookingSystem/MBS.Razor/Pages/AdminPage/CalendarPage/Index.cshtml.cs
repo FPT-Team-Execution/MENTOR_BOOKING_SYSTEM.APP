6ac888f62ed5198583afcf9ec9897c9b70fc4ddb
DatSung <datsung.dev@gmail.com>
2024-10-20 08:58:37 +0700

    feat: build foudnation for all page

---
+++ MentorBookingSystem/MBS.Razor/Pages/AdminPage/CalendarPage/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MBS.Razor.Pages.AdminPage.CalendarPage;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ MentorBookingSystem/MBS.Razor/Pages/AdminPage/ProjectPage/Index.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MBS.Razor.Pages.AdminPage.ProjectPage;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
