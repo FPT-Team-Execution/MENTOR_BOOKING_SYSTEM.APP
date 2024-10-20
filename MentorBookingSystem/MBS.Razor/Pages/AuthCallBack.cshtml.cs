9ebd1d46405fa9be62d713fb3bef09c9006dd02d
NguyenDat0809 <datngx.dev@gmail.com>
2024-10-20 09:51:21 +0700

    feat: prepare for google login & callback [datdev] | 10202024

---
+++ MentorBookingSystem/MBS.Razor/Pages/AuthCallBack.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MBS.Razor.Pages;
+
+public class AuthCallBack : PageModel
+{
+    public string State { get; set; }
+    public string Code { get; set; }
+    public string Scope { get; set; }
+
+    public void OnGet(string state, string code, string scope)
+    {
+        // Lưu hoặc xử lý state, code, và scope ở đây
+        State = state;
+        Code = code;
+        Scope = scope;
+
+        // Sau đó bạn có thể dùng code để lấy token từ Google, ví dụ:
+        // TokenResponse token = await GoogleAuthorizationCodeFlow.ExchangeCodeForTokenAsync(code);
+    }
+}
--- MentorBookingSystem/MBS.Razor/Pages/Login.cshtml.cs
+++ MentorBookingSystem/MBS.Razor/Pages/Login.cshtml.cs
@@ -26,5 +26,29 @@ namespace MBS.Razor.Pages
         {
             
         }
+        /// <summary>
+        /// Redirect to google sign in
+        /// </summary>
+        public void OnGetLoginWithGoogle()
+        {
+            var clientId = "1096581745243-bj51g3cd4rq13codonsoftbk8h7tq4mi.apps.googleusercontent.com";
+            var redirectUrl = "http://localhost:7102/AuthCallBack";
+            //better solution
+            // var redirectUrl = "http://localhost:7102/Login?handler=callback";
+
+            var state = Guid.NewGuid().ToString();
+            var scope = "openid email profile";
+            var responseType = "code";
+            var googleAuthUrl = $"https://accounts.google.com/o/oauth2/v2/auth?redirect_uri={redirectUrl}&prompt=consent&response_type={responseType}&" +
+                                $"client_id={clientId}&scope={scope}&access_type=offline&state={state}";
+            Response.Redirect(googleAuthUrl);
+        }
+        /// <summary>
+        /// Google Callback uri by name handler
+        /// </summary>
+        public void OnGetCallback(string code, string state, string scopes)
+        {
+            Response.Redirect("/Login");
+        }
     }
 }
