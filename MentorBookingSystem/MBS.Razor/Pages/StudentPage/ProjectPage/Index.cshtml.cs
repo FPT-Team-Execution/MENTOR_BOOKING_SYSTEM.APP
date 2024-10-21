44ad32f638d1edf8e554e50cbf21f0503b7424b8
DatSung <datsung.dev@gmail.com>
2024-10-21 16:07:03 +0700

    fixing

---
+++ MentorBookingSystem/MBS.Razor/Pages/StudentPage/ProjectPage/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MBS.Razor.Pages.StudentPage.ProjectPage;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
