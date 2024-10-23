2fc2c1c90ead6aeb5212f187b4322b7bc3c9c2d5
NguyenDat0809 <datngx.dev@gmail.com>
2024-10-23 10:09:44 +0700

    fix: total page is 0 [datdev] | 10232024

--- MentorBookingSystem/MBS.Razor/Pages/AdminPage/StudentPage/Index.cshtml.cs
+++ MentorBookingSystem/MBS.Razor/Pages/AdminPage/StudentPage/Index.cshtml.cs
@@ -10,42 +10,43 @@ namespace MBS.Razor.Pages.AdminPage.StudentPage;
 
 public class Index : PageModel
 {
-    
     public StudentModel ChosenStudent { get; set; } = new();
-    [BindProperty]  public List<StudentModel> Students { get; set; } = new();
+    public List<StudentModel> Students { get; set; } = new();
     private readonly IStudentService _studentService;
     public Index(IStudentService studentService)
     {
         _studentService = studentService;
     }
-    
-    public async Task OnGet()
+
+    public async Task<IActionResult> OnGet()
     {
         var data = await _studentService.GetStudentsAsync(page: 1, size: 10);
         var data2 = data.Items.Select(s => new StudentModel()
-            {
-                Id = s.Id,
-                FullName = s.FullName,
-                Email = s.Email,
-                University = s.University,
-                MajorName = s.MajorId,
-                WalletPoint = s.WalletPoint,
-                AvatarUrl = s.AvatarUrl,
-                Gender = s.Gender,
-                Birthday = s.Birthday,
-                LockoutEnabled = s.LockoutEnabled,
-                UserName = s.UserName,
-                PhoneNumber = s.PhoneNumber,
-                EmailConfirmed = s.EmailConfirmed,
-                LockoutEnd = s.LockoutEnd,
-                CreatedBy = s.CreatedBy,
-                CreatedOn = s.CreatedOn,
-                UpdatedBy = s.UpdatedBy,
-                UpdatedOn = s.UpdatedOn
-            })
+        {
+            Id = s.Id,
+            FullName = s.FullName,
+            Email = s.Email,
+            University = s.University,
+            MajorName = s.MajorId,
+            WalletPoint = s.WalletPoint,
+            AvatarUrl = s.AvatarUrl,
+            Gender = s.Gender,
+            Birthday = s.Birthday,
+            LockoutEnabled = s.LockoutEnabled,
+            UserName = s.UserName,
+            PhoneNumber = s.PhoneNumber,
+            EmailConfirmed = s.EmailConfirmed,
+            LockoutEnd = s.LockoutEnd,
+            CreatedBy = s.CreatedBy,
+            CreatedOn = s.CreatedOn,
+            UpdatedBy = s.UpdatedBy,
+            UpdatedOn = s.UpdatedOn
+        })
             .OrderBy(s => s.FullName)
             .ToList();
-        Students = data2;    
+        Students = data2;
+        TempData["Students"] = Students;
+        return Page();
     }
 
     public async Task<IActionResult> OnGetShowStudentDetail(string studentId)
