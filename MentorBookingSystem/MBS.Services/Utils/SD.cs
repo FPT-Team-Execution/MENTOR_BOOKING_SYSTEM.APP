204c2173a4a0c484c590eb4fa870a226c7cb3b24
NguyenDat0809 <datngx.dev@gmail.com>
2024-10-24 23:06:54 +0700

    update: add key [datdev] | 10242024

--- MentorBookingSystem/MBS.Services/Utils/SD.cs
+++ MentorBookingSystem/MBS.Services/Utils/SD.cs
@@ -4,13 +4,14 @@ public class TempDataKeys
 {
     public const string SuccessMessage = "SuccessMessage";
     public const string ErrorMessage = "ErrorMessage";
-    #region Admin Section
 
+    #region Admin Section
     #region Student Page
 
     public const string StudentPagination = "StudentPagination";
+    public const string ChosenStudent = "ChosenStudent";
+    public const string Majors = "Majors";
 
     #endregion
-
     #endregion
 }
