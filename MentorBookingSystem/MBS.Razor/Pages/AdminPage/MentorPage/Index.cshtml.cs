904cd2406a2f7159f97ed51314455952fee7b952
DatSung <datsung.dev@gmail.com>
2024-10-24 14:01:59 +0700

    feat: get mentors

--- MentorBookingSystem/MBS.Razor/Pages/AdminPage/MentorPage/Index.cshtml.cs
+++ MentorBookingSystem/MBS.Razor/Pages/AdminPage/MentorPage/Index.cshtml.cs
@@ -17,7 +17,8 @@ public class Index : PageModel
     }
 
     public Pagination<MentorResponse>? MentorData { get; set; }
-    
+    public MentorResponse? SelectedMentor { get; set; }
+
     public async Task<ActionResult> OnGet(int page = 1, int size = 10)
     {
         var response = await _mentorService.GetMentorsAsync(page, size) as BaseModel<Pagination<MentorResponse>>;
@@ -29,5 +30,28 @@ public class Index : PageModel
 
         MentorData = response.ResponseRequestModel;
         return Page();
+    }
+
+    public async Task<ActionResult> OnGetShowMentorDetails(string mentorId)
+    {
+        try
+        {
+            var response = await _mentorService.GetMentorsAsync(1, 100) as BaseModel<Pagination<MentorResponse>>;
+            if (response.StatusCode != 200)
+            {
+                TempData["ErrorMessage"] = response.Message;
+                return Page();
+            }
+
+            MentorData = response.ResponseRequestModel;
+
+            SelectedMentor = MentorData?.Items.FirstOrDefault(x => x.Id == mentorId);
+            return Page();
+        }
+        catch (Exception e)
+        {
+            TempData["ErrorMessage"] = e.Message;
+            return Page();
+        }
     }
 }
