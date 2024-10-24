4fc19b8ef423f46a7e50e058161b12483245179d
NguyenDat0809 <datngx.dev@gmail.com>
2024-10-24 22:08:42 +0700

    feat: create SD to save tempdata key [datdev] | 10242024

---
+++ MentorBookingSystem/MBS.Services/Utils/SD.cs
@@ -0,0 +1,14 @@
+namespace MBS.Services.Utils;
+
+public class TempDataKeys
+{
+    #region Admin Section
+
+    #region Student Page
+
+    public const string StudentPagination = "StudentPagination";
+
+    #endregion
+
+    #endregion
+}
