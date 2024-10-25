231398d8e1cee2d69b3a61690f905fbae554ff80
NguyenDat0809 <datngx.dev@gmail.com>
2024-10-25 11:14:56 +0700

    update: add more keys  [datdev] | 10252024

--- MentorBookingSystem/MBS.Services/Utils/SD.cs
+++ MentorBookingSystem/MBS.Services/Utils/SD.cs
@@ -2,16 +2,23 @@ namespace MBS.Services.Utils;
 
 public class TempDataKeys
 {
+    //* Notification
     public const string SuccessMessage = "SuccessMessage";
     public const string ErrorMessage = "ErrorMessage";
+    //* Pagination & Search & Sort 
+    public const string SortOrder = "SortOrder";
+    public const string SearchName = "SearchName";
+    
+    public const string PageIndex = "PageIndex";
+    public const string PageSize = "PageSize";
+    public class AdminKeys
+    {
+        #region Student Page
 
-    #region Admin Section
-    #region Student Page
-
-    public const string StudentPagination = "StudentPagination";
-    public const string ChosenStudent = "ChosenStudent";
-    public const string Majors = "Majors";
-
-    #endregion
-    #endregion
+        public const string StudentPagination = "StudentPagination";
+        public const string ChosenStudent = "ChosenStudent";
+        public const string Majors = "Majors";
+        
+        #endregion
+    }
 }
