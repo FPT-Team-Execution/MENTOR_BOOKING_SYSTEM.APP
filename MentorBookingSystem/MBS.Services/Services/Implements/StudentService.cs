e0fc42facc148b30fc4b59e04a4485b2a8a54eee
NguyenDat0809 <datngx.dev@gmail.com>
2024-10-30 08:49:02 +0700

    feat: create update service [datdev] | 10302024

--- MentorBookingSystem/MBS.Services/Services/Implements/StudentService.cs
+++ MentorBookingSystem/MBS.Services/Services/Implements/StudentService.cs
@@ -1,5 +1,6 @@
 using MBS.Services.Constants;
 using MBS.Services.Models;
+using MBS.Services.Models.Requests.Student;
 using MBS.Services.Models.Responses.Major;
 using MBS.Services.Models.Responses.Student;
 using MBS.Services.Services.Interfaces;
@@ -11,7 +12,7 @@ public class StudentService : IStudentService
 {
     public async Task<Pagination<StudentResponse>> GetStudentsAsync(int page, int size, string sortOrder = "asc")
     {
-        var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiYWRtaW5AZ21haWwuY29tIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZWlkZW50aWZpZXIiOiI4ZmE3YzdiYi1kYWE1LWE2NjAtYmYwMi04MjMwMWE1ZWIzMjciLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJBZG1pbiIsIm5iZiI6MTcyOTU5ODc4OCwiZXhwIjoxNzI5NjAyMzg4LCJpc3MiOiJtYnMtYmFja2VuZCIsImF1ZCI6Im1icy13ZWJhcHAifQ.TMFMdWBdEVXjcnUVZwulRqh78p54xmZAWzk033o-H_M";
+        var token = WebUtils.AccessToken;
         var result = await WebUtils.GetAsync
         (
             ApiEndPoints.StudentUrl,
@@ -30,5 +31,23 @@ public class StudentService : IStudentService
         );
         var response = WebUtils.HandleResponse<BaseModel<Pagination<StudentResponse>>>(result);
         return response.ResponseRequestModel;
+    }
+
+    public async Task<BaseModel<UpdateStudentResponse>> UpdateStudentAsync(UpdateStudentRequest student)
+    {
+        var token = WebUtils.AccessToken;
+        var result = await WebUtils.PutAsync
+        (
+            ApiEndPoints.StudentUpdateUrl,
+            data: student,
+            headers: new Dictionary<string, string>
+            {
+                { "Accept-Charset", "utf-8" },
+                { "Authorization", $"Bearer {token}" }
+            },
+            token: token
+        );
+        var response = WebUtils.HandleResponse<BaseModel<UpdateStudentResponse>>(result);
+        return response;
     }
 }
--- MentorBookingSystem/MBS.Services/Services/Interfaces/IStudentService.cs
+++ MentorBookingSystem/MBS.Services/Services/Interfaces/IStudentService.cs
@@ -1,4 +1,5 @@
 using MBS.Services.Models;
+using MBS.Services.Models.Requests.Student;
 using MBS.Services.Models.Responses.Student;
 
 namespace MBS.Services.Services.Interfaces;
@@ -6,4 +7,6 @@ namespace MBS.Services.Services.Interfaces;
 public interface IStudentService
 {
     Task<Pagination<StudentResponse>> GetStudentsAsync(int page, int size, string sortOrder);
+    Task<BaseModel<UpdateStudentResponse>> UpdateStudentAsync(UpdateStudentRequest student);
+
 }
