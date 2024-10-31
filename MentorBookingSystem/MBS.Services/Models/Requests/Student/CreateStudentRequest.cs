1ece0f314e93ea7c20e966a92a8cc59078ced2b1
NguyenDat0809 <datngx.dev@gmail.com>
2024-10-31 19:17:07 +0700

    feat: create student request model [datdev] | 10312024

--- MentorBookingSystem/MBS.Services/Models/Requests/Student/CreateStudentRequest.cs
+++ MentorBookingSystem/MBS.Services/Models/Requests/Student/CreateStudentRequest.cs
@@ -3,7 +3,7 @@ namespace MBS.Services.Models.Requests.Student;
 public class CreateStudentRequest
 {
     public required string Email { get; set; }
-    public required string Password { get; set; }
+    public required string Password { get; set; } = "123456789aA!";
     public required string FullName { get; set; }
     public required string Gender { get; set; }
     public Guid MajorId { get; set; }
