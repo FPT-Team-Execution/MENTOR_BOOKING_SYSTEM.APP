3e83cbcebba9a4199c0ee731ba609b08b1478a5a
NguyenDat0809 <datngx.dev@gmail.com>
2024-11-11 22:43:42 +0700

    feat: add group dto [datdev] | 11112024

---
+++ MentorBookingSystem/MBS.Services/Dtos/GroupDto.cs
@@ -0,0 +1,12 @@
+namespace MBS.Services.Dtos;
+
+public class GroupDto
+{
+    public Guid id {get; set;}
+    public Guid ProjectId {get; set;}
+    public string ProjectName { get; set; }
+    public string StudentId { get; set;}
+    public string StudentName { get; set; }
+    public Guid PositionId { get; set;}
+    public string PositionName { get; set; }
+}
