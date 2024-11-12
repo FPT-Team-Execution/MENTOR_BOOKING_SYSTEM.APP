532ea101cae7258e7acbd40409025c8445ecc747
DatSung <datsung.dev@gmail.com>
2024-11-12 19:28:25 +0700

    chore: get major

--- MentorBookingSystem/MBS.Razor/Mappers/MajorMapper.cs
+++ MentorBookingSystem/MBS.Razor/Mappers/MajorMapper.cs
@@ -1,17 +1,21 @@
-﻿
-using MBS.Services.Models.Responses.Major;
+﻿using MBS.Services.Models.Responses.Major;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Mapster;
+using MBS.BusinessObject.Entities;
+using MBS.Services.Dtos;
 
 namespace MBS.Razor.Mappers
 {
-    public class MajorMapper
+    public class MajorMapper : IRegister
     {
-        public MajorMapper()
+        public void Register(TypeAdapterConfig config)
         {
+            config.NewConfig<MentorMajor, MajorDto>()
+                .Map(dest => dest.Name, src => src.Major.Name);
         }
     }
 }
