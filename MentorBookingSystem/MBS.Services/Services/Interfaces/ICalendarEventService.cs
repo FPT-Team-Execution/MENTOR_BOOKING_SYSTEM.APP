35041d0ce7801b5995a700d1e86b60996b33a46a
Pham Hoang Minh Khoi <phamkhoi172003@gmail.com>
2024-11-12 18:19:05 +0700

    commit

---
+++ MentorBookingSystem/MBS.Services/Services/Interfaces/ICalendarEventService.cs
@@ -0,0 +1,23 @@
+﻿using MBS.BusinessObject.Entities;
+using MBS.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBS.Services.Services.Interfaces
+{
+    public interface ICalendarEventService
+    {
+        //Task<BaseModel<CreateCalendarResponseModel, CreateCalendarRequestModel>> CreateCalendarEvent(CreateCalendarRequestModel request);
+        //Task<BaseModel<Pagination<CalendarEvent>>> GetCalendarEventsByMentorId(string mentorId, string accessToken, CalendarEventPaginationQueryParameters parameters);
+        //Task<BaseModel<CalendarEventResponseModel>> GetCalendarEventId(string calendarEventId);
+        //Task<BaseModel<GetBusyEventResponse, GetBusyEventRequest>> GetBusyEvent(GetBusyEventRequest request);
+        //Task<BaseModel<UpdateCalendarEventResponseModel>> UpdateCalendarEvent(string calendarEventId, string accessToken, UpdateCalendarEventRequestModel request);
+        //Task<BaseModel> DeleteCalendarEvent(string calendarEventId);
+        //Task<BaseModel<CreateCalendarEventOneFlowResponse, CreateCalendarEventOneFlowRequest>> CreateCalendarEventOnelFlow(CreateCalendarEventOneFlowRequest request);
+
+
+    }
+}
