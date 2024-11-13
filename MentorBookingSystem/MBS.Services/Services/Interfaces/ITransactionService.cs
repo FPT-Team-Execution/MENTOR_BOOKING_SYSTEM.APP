4d0598f548e6592ecc94262cf298dfd46dcbc100
DatVT36 <vuthanhdat181@gmail.com>
2024-11-13 17:45:36 +0700

    feat: add transaction service

---
+++ MentorBookingSystem/MBS.Services/Services/Interfaces/ITransactionService.cs
@@ -0,0 +1,14 @@
+﻿using MBS.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBS.Services.Services.Interfaces
+{
+    public interface ITransactionService
+    {
+        Task<IResponse> PostResponseAsync(IResponse response);
+    }
+}
