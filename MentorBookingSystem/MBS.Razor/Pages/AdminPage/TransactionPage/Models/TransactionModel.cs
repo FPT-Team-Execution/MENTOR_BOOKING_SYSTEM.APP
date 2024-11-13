b3a9475c9b71b0799344cec34134e3fc6add8362
DatVT36 <vuthanhdat181@gmail.com>
2024-11-13 17:43:11 +0700

    feat: add transaction model [DatVT]

---
+++ MentorBookingSystem/MBS.Razor/Pages/AdminPage/TransactionPage/Models/TransactionModel.cs
@@ -0,0 +1,11 @@
+﻿using MBS.Services.Constants.Enums;
+
+namespace MBS.Razor.Pages.AdminPage.TransactionPage.Models
+{
+    public class TransactionModel
+    { 
+        public string StudentId { get; set; }
+        public int Amout { get; set; }
+        public TransactionTypeEnum TransactionTypeEnum { get; set; }
+    }
+}
