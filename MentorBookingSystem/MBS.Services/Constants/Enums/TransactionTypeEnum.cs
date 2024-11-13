2b89aacfe808db94dd20752ef2f87112943d3115
DatVT36 <vuthanhdat181@gmail.com>
2024-11-13 17:43:51 +0700

    feat: add transaction type

---
+++ MentorBookingSystem/MBS.Services/Constants/Enums/TransactionTypeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBS.Services.Constants.Enums
+{
+    public enum TransactionTypeEnum
+    {
+        Credit,
+        Debit
+    }
+}
