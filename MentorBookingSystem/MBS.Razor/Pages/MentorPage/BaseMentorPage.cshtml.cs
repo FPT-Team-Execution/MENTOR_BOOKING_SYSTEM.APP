253adf7f4f329c86ed85662ba84b4c8cd4d2adcf
ThoNT16 <truongthonguyen107@gmail.com>
2024-11-13 13:23:16 +0700

    feat: add mentor project page [thont]

---
+++ MentorBookingSystem/MBS.Razor/Pages/MentorPage/BaseMentorPage.cshtml.cs
@@ -0,0 +1,90 @@
+using MBS.Services.Constants;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+
+namespace MBS.Razor.Pages.MentorPage;
+
+public class BaseMentorPage : PageModel
+{
+    /// <summary>
+    /// Get temp data and keep for next use
+    /// </summary>
+    /// <param name="key">key of saved temp data</param>
+    /// <typeparam name="T">cast result</typeparam>
+    /// <returns>return object in case success, null in case exception</returns>
+    public T? GetTempData<T>(string key, bool isKeep = true) where T : class
+    {
+        try
+        {
+            var data = JsonConvert.DeserializeObject<T>((string)(TempData[key] ?? ""));
+            if (isKeep)
+                TempData.Keep(key);
+            return data;
+        }
+        catch (Exception e)
+        {
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Save value to temp data as string
+    /// </summary>
+    /// <param name="key">key</param>
+    /// <param name="data">object</param>
+    public void SaveTempDataString(string key, string data)
+    {
+        TempData[key] = data;
+    }
+
+    /// <summary>
+    /// Save value to temp data as string
+    /// </summary>
+    /// <param name="key">key</param>
+    /// <param name="data">object</param>
+    public void SaveTempData(string key, object? data)
+    {
+        TempData[key] = JsonConvert.SerializeObject(data);
+    }
+
+    /// <summary>
+    /// Keep data of key list
+    /// </summary>
+    /// <param name="keys"></param>
+    public void KeepTempData(params string[] keys)
+    {
+        foreach (var key in keys)
+        {
+            TempData.Keep(key);
+        }
+    }
+
+    /// <summary>
+    /// Keep All data
+    /// </summary>
+    public void KeepAllTempData()
+    {
+        TempData.Keep();
+    }
+
+    /// <summary>
+    /// Remove Temop Data
+    /// </summary>
+    public void RemoveTempData(params string[] keys)
+    {
+        if (keys.Any())
+        {
+            foreach (var key in keys)
+            {
+                TempData.Remove(key);
+            }
+
+            return;
+        }
+
+        TempData.Clear();
+    }
+    
+   
+}
--- MentorBookingSystem/MBS.Razor/Pages/MentorPage/ProjectPage/Index.cshtml.cs
+++ MentorBookingSystem/MBS.Razor/Pages/MentorPage/ProjectPage/Index.cshtml.cs
@@ -1,11 +1,105 @@
+using Mapster;
+using MBS.DataAccess.Pagination;
+using MBS.Razor.Pages.AdminPage.ProjectPage.Models;
+using MBS.Services.Constants;
+using MBS.Services.Services.Interfaces;
+using MBS.Services.Utils.Shared;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MBS.Razor.Pages.MentorPage.ProjectPage;
 
-public class Index : PageModel
+public class Index : BaseMentorPage
 {
-    public void OnGet()
+    public Pagination<ProjectModel> ProjectPagination { get; set; } = new();
+    [BindProperty] public ProjectModel ChosenProject { get; set; } = new();
+
+    public string SortOrder { get; set; } = "asc";
+    public string search { get; set; } = "";
+
+    public int Size { get; set; } = 5;
+    public int PageIndex { get; set; } = 1;
+
+
+    private readonly IProjectService _projectService;
+
+    public Index(IProjectService projectService)
     {
-        
+        _projectService = projectService;
+    }
+
+    private async Task LoadProject()
+    {
+        var response = await _projectService.GetProjectAsync(PageIndex, Size, search);
+        var projectList = response.Adapt<Pagination<ProjectModel>>();
+        ProjectPagination = projectList;
+
+        SaveTempData(TempDataKeys.AdminKeys.ProjectPagination, ProjectPagination);
+        SaveTempData(TempDataKeys.PageIndex, PageIndex);
+        SaveTempData(TempDataKeys.PageSize, Size);
+
+    }
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        try
+        {
+            await LoadProject();
+        }
+        catch
+        {
+            SaveTempData(TempDataKeys.ErrorMessage, "Some error occurred");
+            return RedirectToPage(RouteEndpoints.AdminProject);
+        }
+
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostPageNavigate(string pageIndex, string size)
+    {
+        try
+        {
+            var projectPagination = GetTempData<Pagination<ProjectModel>>(TempDataKeys.AdminKeys.ProjectPagination)!;
+            //set pageIndex and page Size
+            Size = int.Parse(size);
+            //if total item from previous load * previous total pages is lower or equal then new size -> pageIndex = 1
+            if ((projectPagination.TotalItems * projectPagination.TotalItems) <= Size)
+                PageIndex = 1;
+            else
+                PageIndex = int.Parse(pageIndex);
+            //Save temp data to next use
+            SaveTempData(TempDataKeys.PageIndex, PageIndex);
+            SaveTempData(TempDataKeys.PageSize, Size);
+            //Load data pagination from api
+            await LoadProject();
+        }
+        catch (Exception e)
+        {
+            SaveTempDataString(TempDataKeys.ErrorMessage, "Some error occurred");
+            Redirect(RouteEndpoints.AdminStudent);
+        }
+
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostSearch(string search)
+    {
+        try
+        {
+            this.search = search;
+            SaveTempData("search", search);
+            //Save temp data to next use
+            SaveTempData(TempDataKeys.PageIndex, 1);
+            SaveTempData(TempDataKeys.PageSize, 2);
+            //Load data pagination from api
+            await LoadProject();
+        }
+        catch (Exception e)
+        {
+            SaveTempDataString(TempDataKeys.ErrorMessage, "Some error occurred");
+            Redirect(RouteEndpoints.AdminStudent);
+        }
+
+        return Page();
     }
 }
