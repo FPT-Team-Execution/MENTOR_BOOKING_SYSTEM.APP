a9bea156b3e5dd2eeb08565e1e25e7817f572fb5
DatVT36 <vuthanhdat181@gmail.com>
2024-11-13 17:46:19 +0700

    feat: add base Request service

---
+++ MentorBookingSystem/MBS.Services/Services/Interfaces/IRequestService.cs
@@ -0,0 +1,14 @@
+﻿using MBS.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBS.Services.Services.Interfaces
+{
+    public interface IRequestService
+    {
+        Task<IResponse> GetResponseAsync(int page, int size, string sortOrder);
+    }
+}
