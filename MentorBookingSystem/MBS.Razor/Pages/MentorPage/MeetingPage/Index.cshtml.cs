2a8ffbc00a4a5e3d90947074b8118f1d45372490
Pham Hoang Minh Khoi <phamkhoi172003@gmail.com>
2024-11-15 23:10:10 +0700

    UI

--- MentorBookingSystem/MBS.Razor/Pages/MentorPage/MeetingPage/Index.cshtml.cs
+++ MentorBookingSystem/MBS.Razor/Pages/MentorPage/MeetingPage/Index.cshtml.cs
@@ -1,11 +1,11 @@
+using MBS.DataAccess.Pagination;
+using MBS.Services.Dtos;
+using MBS.Services.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MBS.Razor.Pages.MentorPage.MeetingPage;
 
-public class Index : PageModel
+public class IndexModel : PageModel
 {
-    public void OnGet()
-    {
-        
-    }
+   
 }
