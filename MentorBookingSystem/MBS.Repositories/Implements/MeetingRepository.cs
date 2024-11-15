ff97816ed03a21535ff3c099cdcce6d49e161952
ThoNT16 <truongthonguyen107@gmail.com>
2024-11-15 23:42:17 +0700

    fix: wrong predicate meeting [thont]

--- MentorBookingSystem/MBS.Repositories/Implements/MeetingRepository.cs
+++ MentorBookingSystem/MBS.Repositories/Implements/MeetingRepository.cs
@@ -30,7 +30,7 @@ namespace MBS.Repositories.Implements
 
         public async Task<Meeting?> GetMeetingByRequestId(string requestId)
         {
-            return await  _dao.SingleOrDefaultAsync(meeting => meeting.Id.ToString() == requestId );
+            return await _dao.SingleOrDefaultAsync(meeting => meeting.RequestId.ToString() == requestId );
         }
 
         public async Task<IEnumerable<Meeting>> GetMeetingsByRequests(IEnumerable<Guid> requestIds)
