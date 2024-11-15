8d7d66b558f3db6cf38a92112ac0d0d36cc4231a
Pham Hoang Minh Khoi <phamkhoi172003@gmail.com>
2024-11-15 23:10:42 +0700

    backend

--- MentorBookingSystem/MBS.Razor/Pages/MentorPage/MeetingPage/Index.cshtml.cs
+++ MentorBookingSystem/MBS.Razor/Pages/MentorPage/MeetingPage/Index.cshtml.cs
@@ -7,5 +7,22 @@ namespace MBS.Razor.Pages.MentorPage.MeetingPage;
 
 public class IndexModel : PageModel
 {
-   
+    private readonly IMeetingService _meetingService;
+
+    public Pagination<MeetingDto> MeetingPagination { get; set; } = new();
+    public int PageSize { get; set; } = 5;
+    public int PageIndex { get; set; } = 1;
+
+    public IndexModel(IMeetingService meetingService)
+    {
+        _meetingService = meetingService;
+    }
+
+    public async Task OnGetAsync(int pageIndex = 1, int pageSize = 5)
+    {
+        PageIndex = pageIndex;
+        PageSize = pageSize;
+
+        MeetingPagination = await _meetingService.GetMeetingsPaginationAsync("", pageIndex, pageSize);
+    }
 }
