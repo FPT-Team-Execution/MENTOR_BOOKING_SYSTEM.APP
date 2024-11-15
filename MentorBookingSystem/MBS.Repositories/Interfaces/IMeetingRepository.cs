818db319996d961e226b2d015d59cb84b6296824
Pham Hoang Minh Khoi <phamkhoi172003@gmail.com>
2024-11-15 22:48:34 +0700

    repo and service prepare

--- MentorBookingSystem/MBS.Repositories/Interfaces/IMeetingRepository.cs
+++ MentorBookingSystem/MBS.Repositories/Interfaces/IMeetingRepository.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MBS.DataAccess.Pagination;
 
 namespace MBS.Repositories.Interfaces
 {
@@ -12,6 +13,7 @@ namespace MBS.Repositories.Interfaces
         Task<IEnumerable<Meeting>> GetMeetingsByRequests(IEnumerable<Guid> requestIds);
         Task<IEnumerable<Meeting>> GetMeetingsByRequest(Guid requestId);
 
+        Task<Pagination<Meeting>> GetPageListMeeting(int page, int size);
 
     }
 }
--- MentorBookingSystem/MBS.Services/Services/Implements/MeetingService.cs
+++ MentorBookingSystem/MBS.Services/Services/Implements/MeetingService.cs
@@ -10,17 +10,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MBS.Repositories.Interfaces;
+using MBS.Services.Dtos;
 
 namespace MBS.Services.Services.Implements
 {
     public class MeetingService : IMeetingService
     {
+        private readonly IMeetingRepository _meetingRepository;
+
+        public MeetingService(IMeetingRepository meetingRepository)
+        {
+            _meetingRepository = meetingRepository;
+        }
         public async Task<IResponse> GetMeetingAsync(int page, int size)
         {
             var url = ApiEndPoints.Meeting.Replace("{page}", page.ToString()).Replace("{size}", size.ToString());
             var result = await WebUtils.GetAsync(url,token: WebUtils.AccessToken);
             var response = WebUtils.HandleResponse<BaseModel<Pagination<MeetingResponse>>>(result);
             return response;
+        }
+
+        public async Task<Pagination<MeetingDto>> GetMeetingsPaginationAsync(string meetingId, int page, int size)
+        {
+            var result = await _meetingRepository.
+        }
+        
+        public async Task<Pagination<MajorDto>> GetMentorMajorsAsync(string mentorId, int page, int size)
+        {
+            var result = await _meetingRepository.Ge(mentorId, page, size);
+            return result.Adapt<Pagination<MajorDto>>();
         }
     }
 }
