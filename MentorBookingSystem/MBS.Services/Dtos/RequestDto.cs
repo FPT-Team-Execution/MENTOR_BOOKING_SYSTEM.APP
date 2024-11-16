f709b064f35378f8d877339e45c071b245d0b2cb
ThoNT16 <truongthonguyen107@gmail.com>
2024-11-16 16:25:19 +0700

    fix: fix project name require [thont]

--- MentorBookingSystem/MBS.Services/Dtos/RequestDto.cs
+++ MentorBookingSystem/MBS.Services/Dtos/RequestDto.cs
@@ -24,7 +24,7 @@ public class RequestDto
     public string Status { get; set; } = string.Empty;
     public DateTime? CreatedOn { get; set; }
     public string MeetingLink { get; set; } = string.Empty;
-    public string ProjectName { get; set; }
+    public string ProjectName { get; set; } = string.Empty;
 
 
 }
