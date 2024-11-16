61b13ea4251639a022c1eb321ce5846bbecea301
Pham Hoang Minh Khoi <phamkhoi172003@gmail.com>
2024-11-16 11:04:45 +0700

    done script for UI

--- MentorBookingSystem/MBS.Razor/Pages/MentorPage/MeetingPage/Index.cshtml.cs
+++ MentorBookingSystem/MBS.Razor/Pages/MentorPage/MeetingPage/Index.cshtml.cs
@@ -12,20 +12,34 @@ public class IndexModel : PageModel
     public Pagination<MeetingDto> MeetingPagination { get; set; } = new();
     public int PageSize { get; set; } = 5;
     public int PageIndex { get; set; } = 1;
-
-    public string MentorId { get; set; } = string.Empty; 
+    public string MentorId { get; set; } = string.Empty;
 
     public IndexModel(IMeetingService meetingService)
     {
         _meetingService = meetingService;
     }
 
-    public async Task OnGetAsync(string mentorId, int pageIndex = 1, int pageSize = 5)
+    public async Task OnGetAsync(string mentorId = "", int pageIndex = 1, int pageSize = 5)
     {
+        MentorId = mentorId;
         PageIndex = pageIndex;
         PageSize = pageSize;
-        MentorId = mentorId;
 
-        MeetingPagination = await _meetingService.GetMeetingsByMentorIdPaginationAsync(mentorId, pageIndex, pageSize);
+        if (!string.IsNullOrWhiteSpace(mentorId))
+        {
+            MeetingPagination =
+                await _meetingService.GetMeetingsByMentorIdPaginationAsync(mentorId, pageIndex, pageSize);
+        }
+        else
+        {
+            MeetingPagination = new Pagination<MeetingDto>
+            {
+                Items = new List<MeetingDto>(),
+                TotalItems = 0,
+                TotalPages = 0,
+                PageIndex = 1,
+                PageSize = pageSize
+            };
+        }
     }
 }
